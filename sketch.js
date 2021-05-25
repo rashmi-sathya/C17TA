a55af143f889f446afc479a8ebc8fcd60eee4d69
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-25 16:52:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var box1;
+var box2;
+
+function setup() {
+  createCanvas(600, 400);
+  box1 = new Box(100,100,50,50);
+  //Add the object for box2
+
+}
+
+
+function draw() {
+  background(220);
+  box1.show();
+//display box2
+  
+}
+
